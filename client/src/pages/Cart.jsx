f7b48ab498a0b3a20080dcdbd3d704d86e4b7b75
Arihant Jain <127401868+Arihantjain04@users.noreply.github.com>
2024-06-24 21:03:31 +0530

    deploying

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -13,7 +13,7 @@ const Cart = () => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8000/api/product/cart/${userid}`,
+          `https://e-commerce-tkjz.onrender.com/api/product/cart/${userid}`,
           {
             method: "GET",
             headers: { "Content-Type": "application/json" },
