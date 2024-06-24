37943eb2ca31d55536fcee151f142ffe36bdcaf9
Arihant Jain <127401868+Arihantjain04@users.noreply.github.com>
2024-06-24 21:11:00 +0530

    deploying

--- client/src/pages/Admin/UpdateProduct.jsx
+++ client/src/pages/Admin/UpdateProduct.jsx
@@ -23,7 +23,7 @@ const UpdateProduct = () => {
     const fetchProduct = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8000/api/product/getProduct/${id}`
+          `https://e-commerce-tkjz.onrender.com/api/product/getProduct/${id}`
         );
         if (!response.ok) {
           throw new Error("Failed to fetch product");
